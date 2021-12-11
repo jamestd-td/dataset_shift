d326e8c9e42ff2fd8ddbd10ae645254c0c3bbb06
jamestd-td <61223410+jamestd-td@users.noreply.github.com>
2021-12-11 13:48:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -60,3 +60,45 @@ def confidence_interval_auc(auc, n1, n2):
     AUC_upper = AUC + z * SE_AUC
     
     return (AUC_lower, AUC_upper)
+
+# =============================================================================
+#  Split the given CSV file into train 80%, validation 10% and holdout test 10%
+#  The algorithm use permutation to make sure your dataset split completely random
+#
+#  The argumemnt to pass is data frame 
+# =============================================================================
+
+def train_validate_test_split(df, train_percent=.800000, validate_percent=.1, seed=None):
+    np.random.seed(seed)
+    perm = np.random.permutation(df.index)
+    m = len(df.index)
+    train_end = int(train_percent * m)
+    validate_end = int(validate_percent * m) + train_end
+    train = df.iloc[perm[:train_end]]
+    validate = df.iloc[perm[train_end:validate_end]]
+    test = df.iloc[perm[validate_end:]]
+    return train, validate, test
+
+# =============================================================================
+#  Split the given CSV file into train 80%, validation 10% and holdout test 10%
+#  The algorithm use permutation to make sure your dataset split completely random
+#
+#  The argumemnt to pass is 
+#  data frame = where your data with all files are avilable under "fname" heading
+#  in_path = location of your ROI extracted CXR files
+#  dest_path =  location where you want to move your data
+#
+#  for example your data frame for IN dataset for hould out test for TB patients, 
+#  your CXR is in  segmentation_result dir ( in_path) and move test files to in_test/tb
+#  (dest_path)
+# =============================================================================
+
+def move_files(df, in_path, dest_path):
+    image=df['fname']
+    print(image[0:len(image)])
+    
+    for i in image[0:]: 
+        shutil.move(os.path.join(in_path, i),
+                    dest_path)
+        print(i)
+
